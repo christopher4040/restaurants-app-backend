9084099855357ab0dd4b98efdc26a586a05b38de
Christopher Maldonado <christopherpr101@gmail.com>
2022-10-30 18:06:13 -0500

    Update middleware

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,19 +1,5 @@
 module.exports = ({ env }) => [
   'strapi::errors',
-  // {
-  //   name: 'strapi::security',
-  //   config: {
-  //     contentSecurityPolicy: {
-  //       useDefaults: true,
-  //       directives: {
-  //         'connect-src': ["'self'", 'https:'],
-  //         'img-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
-  //         'media-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
-  //         upgradeInsecureRequests: null,
-  //       },
-  //     },
-  //   },
-  // },
   {
     name: 'strapi::security',
     config: {
@@ -40,7 +26,14 @@ module.exports = ({ env }) => [
       },
     },
   },
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['*']
+    },
+  },
+  // 'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',

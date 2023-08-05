6c02f43fddaff4fd6fd0d5a03bde151322010476
Christopher Maldonado <christopherpr101@gmail.com>
2023-08-05 14:11:52 -0500

    Update - SSL authorization

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS', ['PyrBLzRS2QYrsvDGETLzRw==', 'HG4zWrspdQZIA1dubB5nLw==', 'W60EhXu4qTHD9jR1VSpi7g==', 'SsMXLn4CJTo1ieGY5OejMw==']),
+    keys: env.array('APP_KEYS'),
   },
 });
